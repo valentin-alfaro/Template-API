2e3632375467d11853dce1b2541c34c6a9245253
valentin-alfaro <valentinalfaro51@gmail.com>
2025-05-29 22:49:02 -0300

    Add Startup
    Se reconfigura API para trabajar con middleware de Startup

---
+++ Template-API/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Template_API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            //app.UseCors();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            //app.UseAuthentication();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+
+    }
+}
